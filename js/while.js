bfb93da5c78894adc8ee9f693a4b30287bb31366
SAMJOELSARELLA <samsarella7777@gmail.com>
2019-01-09 21:37:56 +0530

    Array order using While loop

---
+++ js/while.js
@@ -0,0 +1,19 @@
+let Names = ["SAM","JOEL","SAGAR","SARELLA"];
+
+let index = 0;
+
+while(index < Names.length){
+    console.log(Names[index]);
+    index +=1;
+}
+
+
+//reverse of array
+// let Names = ["SAM","JOEL","SAGAR","SARELLA"];
+
+// let index = Names.length-1;
+
+// while(index >= 0){
+//     console.log(Names[index]);
+//     index -= 1;
+// }
